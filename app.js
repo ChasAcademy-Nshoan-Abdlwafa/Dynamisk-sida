3a915c3154491605115c3f124fab63afc4522043
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2022-12-11 13:55:55 +0100

    Sjunde iteration av Dynamisk-sida

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let school1 = document.getElementById("school-1");
+let list1 = document.getElementById("list1");
+let list2 = document.getElementById("list2");
 appendList();
 /* let x = list[8]; */
 function appendList() {
@@ -8,8 +9,8 @@ function appendList() {
     .then((result) => result.json()) // Skapar JSON-data, "jsonifierar"
     .then((content) => {
       // Färdigt JSON-objekt, redo att användas
-
-      for (let i = 0; i < content.todolist.length; i++) {
+      console.log(content);
+      for (let i = 0; i < content.todolist1.length; i++) {
         // Skapar for-loop, som går igenom varje element i listan
         // Första delen (let i = 0;): vart det ska börja iterera, här börjar det på allra första elementet, dvs. i = 0
         // Andra delen (i < content.todolist.length;): när det ska sluta iterera, här avslutas itererationen när den har nått det sista elementet
@@ -17,26 +18,13 @@ function appendList() {
 
         let listItem = document.createElement("li"); // Skapar ett li-objekt i HTML, via JavaScript
 
-        let todoListItem = content.todolist[i];
+        let todoListItem = content.todolist1[i];
         listItem.innerHTML = todoListItem.ToDo; // Populera skapade li-elementet med värdena från JSON-datan
 
-        list.appendChild(listItem); // Lägger till li-element till ul-listan som hämtades in från HTML-dokumentet
+        list1.appendChild(listItem); // Lägger till li-element till ul-listan som hämtades in från HTML-dokumentet
       }
-    });
-}
 
-let school2 = document.getElementById("school-2");
-appendList();
-/* let x = list[8]; */
-function appendList() {
-  return fetch(
-    "https://raw.githubusercontent.com/ChasAcademy-Nshoan-Abdlwafa/Dynamisk-sida/main/data.json"
-  )
-    .then((result) => result.json()) // Skapar JSON-data, "jsonifierar"
-    .then((content) => {
-      // Färdigt JSON-objekt, redo att användas
-
-      for (let i = 0; i < content.todolist.length; i++) {
+      for (let i = 0; i < content.todolist2.length; i++) {
         // Skapar for-loop, som går igenom varje element i listan
         // Första delen (let i = 0;): vart det ska börja iterera, här börjar det på allra första elementet, dvs. i = 0
         // Andra delen (i < content.todolist.length;): när det ska sluta iterera, här avslutas itererationen när den har nått det sista elementet
@@ -44,64 +32,10 @@ function appendList() {
 
         let listItem = document.createElement("li"); // Skapar ett li-objekt i HTML, via JavaScript
 
-        let todoListItem = content.todolist[i];
+        let todoListItem = content.todolist2[i];
         listItem.innerHTML = todoListItem.ToDo; // Populera skapade li-elementet med värdena från JSON-datan
 
-        list.appendChild(listItem); // Lägger till li-element till ul-listan som hämtades in från HTML-dokumentet
-      }
-    });
-}
-
-let workplace1 = document.getElementById("workplace-1");
-appendList();
-/* let x = list[8]; */
-function appendList() {
-  return fetch(
-    "https://raw.githubusercontent.com/ChasAcademy-Nshoan-Abdlwafa/Dynamisk-sida/main/data.json"
-  )
-    .then((result) => result.json()) // Skapar JSON-data, "jsonifierar"
-    .then((content) => {
-      // Färdigt JSON-objekt, redo att användas
-
-      for (let i = 0; i < content.todolist.length; i++) {
-        // Skapar for-loop, som går igenom varje element i listan
-        // Första delen (let i = 0;): vart det ska börja iterera, här börjar det på allra första elementet, dvs. i = 0
-        // Andra delen (i < content.todolist.length;): när det ska sluta iterera, här avslutas itererationen när den har nått det sista elementet
-        // Tredje delen (i++): hur många steg per iteration, här itereras det över varje enskild listelement, så i++ (i += 1)
-
-        let listItem = document.createElement("li"); // Skapar ett li-objekt i HTML, via JavaScript
-
-        let todoListItem = content.todolist[i];
-        listItem.innerHTML = todoListItem.ToDo; // Populera skapade li-elementet med värdena från JSON-datan
-
-        list.appendChild(listItem); // Lägger till li-element till ul-listan som hämtades in från HTML-dokumentet
-      }
-    });
-}
-
-let workplace2 = document.getElementById("workplace-2");
-appendList();
-/* let x = list[8]; */
-function appendList() {
-  return fetch(
-    "https://raw.githubusercontent.com/ChasAcademy-Nshoan-Abdlwafa/Dynamisk-sida/main/data.json"
-  )
-    .then((result) => result.json()) // Skapar JSON-data, "jsonifierar"
-    .then((content) => {
-      // Färdigt JSON-objekt, redo att användas
-
-      for (let i = 0; i < content.todolist.length; i++) {
-        // Skapar for-loop, som går igenom varje element i listan
-        // Första delen (let i = 0;): vart det ska börja iterera, här börjar det på allra första elementet, dvs. i = 0
-        // Andra delen (i < content.todolist.length;): när det ska sluta iterera, här avslutas itererationen när den har nått det sista elementet
-        // Tredje delen (i++): hur många steg per iteration, här itereras det över varje enskild listelement, så i++ (i += 1)
-
-        let listItem = document.createElement("li"); // Skapar ett li-objekt i HTML, via JavaScript
-
-        let todoListItem = content.todolist[i];
-        listItem.innerHTML = todoListItem.ToDo; // Populera skapade li-elementet med värdena från JSON-datan
-
-        list.appendChild(listItem); // Lägger till li-element till ul-listan som hämtades in från HTML-dokumentet
+        list2.appendChild(listItem); // Lägger till li-element till ul-listan som hämtades in från HTML-dokumentet
       }
     });
 }
